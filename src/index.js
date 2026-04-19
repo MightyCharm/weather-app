b553492e9acd52234b4c1a0189d4a4d09a12cbd0
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2026-04-19 16:12:25 +0200

    feat(data): refactor logic and add dynamic fetch

--- src/index.js
+++ src/index.js
@@ -21,18 +21,21 @@ const uiWind = document.querySelector("#data-wind");
 const uiHumidity = document.querySelector("#data-humidity");
 const uiDescription = document.querySelector("#data-description");
 
-async function fetchData() {
+async function fetchData(input) {
+  console.log("function fetchData() fetches and mdifies the data");
+  const city = input;
   try {
     const response = await fetch(
-      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/pforzheim?unitGroup=metric&key=XNT5W4M924BSW324WY2Q8UA4N",
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=XNT5W4M924BSW324WY2Q8UA4N`,
     );
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     console.log(`Successful fetch: ${response.ok} ${response.status}`);
+
     const data = await response.json();
-    localStorage.setItem("data", JSON.stringify(data));
-    return data;
+    const modifiedData = { data: data, timestamp: Date.now() };
+    return modifiedData;
   } catch (error) {
     console.log(error);
   }
@@ -61,6 +64,7 @@ async function extractData(data) {
   const description = data.description;
   const icon = data.currentConditions.icon;
 
+  /*
   console.log(`address: ${address}`);
   console.log(`date: ${date}`);
   console.log(`datetime: ${time}`);
@@ -76,7 +80,7 @@ async function extractData(data) {
   console.log(`uvindex: ${uvindex}`);
 
   console.log(`description: ${description}`);
-
+  */
   uiAddress.textContent = address.charAt(0).toUpperCase() + address.slice(1);
   uiDate.textContent = `${date}`;
   uiTime.textContent = `${time} Uhr`;
@@ -90,18 +94,59 @@ async function extractData(data) {
   uiDescription.textContent = description;
 }
 
-async function init() {
-  console.log("init()");
-  let data = JSON.parse(localStorage.getItem("data"));
-  if (!data) {
-    data = await fetchData();
-  }
-
-  if (data) {
-    extractData(data);
-  } else {
-    console.log("No data available");
-  }
+function getDataStorage() {
+  console.log("function getDataStorage()");
+  const checkData = JSON.parse(localStorage.getItem("data"));
+  return checkData ? checkData : false;
 }
 
-init();
+function setDataStorage(data) {
+  console.log("function setDataStoarage()");
+  localStorage.setItem("data", JSON.stringify(data));
+}
+
+async function updateWeatherUI(input) {
+  console.log("function updateWeatherUI_beta()");
+  let data = getDataStorage(input);
+  let isData = false;
+  let isSameCity = false;
+  let isDataStale = false;
+
+  console.log("data:", data);
+  isData = data ? true : false;
+  console.log("isData", isData);
+
+  if (isData) {
+    console.log("--> A)");
+    const dataAddress = data.data.address;
+    console.log("dataAddress:", dataAddress);
+
+    if (dataAddress === input) {
+      isSameCity = true;
+    }
+
+    const currentTime = Date.now();
+    const timestamp = data.timestamp;
+    console.log("timestamp:", timestamp, typeof timestamp);
+
+    const lastFetchMilliSeconds = currentTime - timestamp;
+    const lastFetchMinutes = Math.ceil(lastFetchMilliSeconds / 1000 / 60);
+    console.log(lastFetchMinutes, lastFetchMilliSeconds);
+    if (lastFetchMinutes > 10) {
+      isDataStale = true;
+    }
+  }
+
+  console.log(
+    `final check\nisData: ${isData}\nisSameCity: ${isSameCity}\nisOldData: ${isDataStale}`,
+  );
+  if (isData === false || isSameCity === false || isDataStale === true) {
+    console.log("new fetch request");
+    data = await fetchData(input);
+    setDataStorage(data);
+  }
+  extractData(data.data);
+}
+
+const input = "pforzheim".toLowerCase();
+updateWeatherUI(input);
