3018b1f33fdff0f9888e8347d4dad295be6e0dec
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2026-05-07 22:09:05 +0200

    feat(form): add input validation for search field

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const btnColorTheme = document.getElementById("btn-color");
 const iconTheme = document.querySelector(".icon-theme");
 // form input elements
 const inputForm = document.getElementById("search");
-const searchButton = document.getElementById("search-btn");
+const form = document.getElementById("fetch-form");
 // current weather
 const uiResolvedAddress = document.getElementById("address");
 const uiCurrentTime = document.getElementById("current-time");
@@ -54,7 +54,6 @@ btnTempUnit.addEventListener("click", () => {
 });
 
 btnColorTheme.addEventListener("click", () => {
-  //console.log(body.getAttribute("data-theme"));
   if (body.getAttribute("data-theme")) {
     setThemeStorage("dark");
   } else {
@@ -63,7 +62,8 @@ btnColorTheme.addEventListener("click", () => {
   setAppTheme();
 });
 
-searchButton.addEventListener("click", (event) => {
+form.addEventListener("submit", (event) => {
+  console.log("eventlistener form");
   event.preventDefault();
   const input = getUserInput();
   updateUI(input);
@@ -174,15 +174,12 @@ function getTime() {
 }
 
 function displayLoadingScreen(show) {
-  console.log("displayLoadingScreen()");
   if (show) {
-    //console.log("A)");
     sectionCurrent.classList.add("hidden");
     sectionForecast.classList.add("hidden");
     sectionExtraInformation.classList.add("hidden");
     uiLoading.classList.remove("hidden");
   } else {
-    //console.log("B)");
     setTimeout(() => {
       sectionCurrent.classList.remove("hidden");
       sectionForecast.classList.remove("hidden");
@@ -193,7 +190,6 @@ function displayLoadingScreen(show) {
 }
 
 function updateMessageLoadingScreen(type) {
-  console.log("updateMessageLoadingScreen()", type);
   if (type === "loading" || type === "fetch") {
     uiParaLoading.textContent = "Loading...";
   } else if (type === "no-data") {
@@ -308,7 +304,6 @@ async function updateUI(input) {
   }
   // check if we have data or not
   if (data) {
-    console.log("if data", data);
     setDataStorage(data);
     updateCurrentUI(data.data);
     updateForecast(data.data.forecasts);
@@ -317,7 +312,6 @@ async function updateUI(input) {
     displayLoadingScreen(false);
     //updateMessageLoadingScreen("step: we have data");
   } else {
-    console.log("else: no data", data);
     updateMessageLoadingScreen("no-data");
   }
 }
